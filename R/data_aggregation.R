38dd32cfcc36b2bc8dab4d56149ecde009c4c1ed
Ela <ElenaScholz>
2024-04-21 13:01:43 +0200

    more updates

--- R/data_aggregation.R
+++ R/data_aggregation.R
@@ -1,80 +1,80 @@
 #' Aggregate Data
 #'
-#' Data aggregation by Day, Month, Season or Year
+#' Perform data aggregation by Day, Month, Season, or Year based on a specified aggregation column.
 #'
-#' @param df MANDATORY Type: dataframe- - the input Dataframe containing the Loggerdata (please apply rename colnames and modify dates before)
-#' @param aggregation_type MANDATORY - Type: String - you can choose between "daily", "monthly", "seasonal" and "annual"
-#' @param temperature_column OPTIONAL - Type: String - if temperature column is not "Temperature_C" change the parameter
+#' @param df A dataframe containing the Logger data. Columns should include "Logger_ID", "Month", "Date", "Year", "Julian", and the specified aggregation column.
+#' @param aggregation_type The type of aggregation to perform: "daily", "monthly", "seasonal", or "annual".
+#' @param aggregation_column The name of the column to use for aggregation.
 #'
-#' @return returns a dataframe with the aggregated temperature data. it will contain new columns for mean, standard derivation, minimum and maximum Temperature
+#' @return A dataframe with the aggregated data including mean, standard deviation, minimum, and maximum of the specified aggregation column.
 #'
-#' @import dplyr
-#' @import lubridate
+#' @importFrom dplyr group_by filter summarise mutate case_when first
 #' @importFrom stats sd
-#'
+#' @importFrom lubridate month year
 #'
 #' @export
 #'
-#'
-aggregate_data <- function(df, aggregation_type, temperature_column) {
-  required_columns <- c("Logger_ID","Month", "Date", "Year", "Julian",temperature_column)
-
+aggregate_data <- function(df, aggregation_type, aggregation_column) {
+  # Check if required columns are present in the dataframe
+  required_columns <- c("Logger_ID", "Month", "Date", "Year", "Julian", aggregation_column)
   if (!all(required_columns %in% colnames(df))) {
     missing_columns <- required_columns[!required_columns %in% colnames(df)]
     stop(paste("Required columns missing in the dataframe:", paste(missing_columns, collapse = ", ")))
   }
 
-  colnames(df)[colnames(df) == temperature_column] <- "Temperature_C"
-
+  # Perform aggregation based on aggregation type
   if (aggregation_type == "daily") {
-    daily_temperature <- dplyr::group_by(df, Date) %>%
-      dplyr::filter(!is.na(Temperature_C)) %>%
-      dplyr::summarise(mean_temperature = mean(Temperature_C),
-                       std_temperature = stats::sd(Temperature_C),
-                       min_temperature = min(Temperature_C),
-                       max_temperature = max(Temperature_C),
+    daily_data <- df %>%
+      dplyr::group_by(Date) %>%
+      dplyr::filter(!is.na({{aggregation_column}})) %>%
+      dplyr::summarise(mean_value = mean({{aggregation_column}}),
+                       std_value = stats::sd({{aggregation_column}}),
+                       min_value = min({{aggregation_column}}),
+                       max_value = max({{aggregation_column}}),
                        Logger_ID = dplyr::first(Logger_ID),
                        Month = dplyr::first(Month),
                        Year = dplyr::first(Year),
                        Julian = dplyr::first(Julian))
-    return(daily_temperature)
+    return(daily_data)
   } else if (aggregation_type == "monthly") {
-    monthly_temperature <- dplyr::group_by(df, Month, Year) %>%
-      dplyr::filter(!is.na(Temperature_C)) %>%
-      dplyr::summarise(mean_temperature = mean(Temperature_C),
-                       std_temperature = stats::sd(Temperature_C),
-                       min_temperature = min(Temperature_C),
-                       max_temperature = max(Temperature_C),
+    monthly_data <- df %>%
+      dplyr::group_by(Month, Year) %>%
+      dplyr::filter(!is.na({{aggregation_column}})) %>%
+      dplyr::summarise(mean_value = mean({{aggregation_column}}),
+                       std_value = stats::sd({{aggregation_column}}),
+                       min_value = min({{aggregation_column}}),
+                       max_value = max({{aggregation_column}}),
                        Logger_ID = dplyr::first(Logger_ID))
-    return(monthly_temperature)
+    return(monthly_data)
   } else if (aggregation_type == "annual") {
-    annual_temperature <- dplyr::group_by(df, Year) %>%
-      dplyr::filter(!is.na(Temperature_C)) %>%
-      dplyr::summarise(mean_temperature = mean(Temperature_C),
-                       std_temperature = stats::sd(Temperature_C),
-                       min_temperature = min(Temperature_C),
-                       max_temperature = max(Temperature_C),
+    annual_data <- df %>%
+      dplyr::group_by(Year) %>%
+      dplyr::filter(!is.na({{aggregation_column}})) %>%
+      dplyr::summarise(mean_value = mean({{aggregation_column}}),
+                       std_value = stats::sd({{aggregation_column}}),
+                       min_value = min({{aggregation_column}}),
+                       max_value = max({{aggregation_column}}),
                        Logger_ID = dplyr::first(Logger_ID),
                        .groups = "drop")
-    return(annual_temperature)
+    return(annual_data)
   } else if (aggregation_type == "seasonal") {
-    seasonal_temperature <- dplyr::mutate(df, Season = dplyr::case_when(
-      lubridate::month(Date) %in% 3:5 ~ "Spring",
-      lubridate::month(Date) %in% 6:8 ~ "Summer",
-      lubridate::month(Date) %in% 9:11 ~ "Autumn",
-      TRUE ~ "Winter"
-    )) %>%
+    seasonal_data <- df %>%
+      dplyr::mutate(Season = dplyr::case_when(
+        lubridate::month(Date) %in% 3:5 ~ "Spring",
+        lubridate::month(Date) %in% 6:8 ~ "Summer",
+        lubridate::month(Date) %in% 9:11 ~ "Autumn",
+        TRUE ~ "Winter"
+      )) %>%
       dplyr::group_by(Year = lubridate::year(Date), Season) %>%
-      dplyr::filter(!is.na(Temperature_C)) %>%
-      dplyr::summarise(mean_temperature = mean(Temperature_C),
-                       std_temperature = stats::sd(Temperature_C),
-                       Tmin_temperature = min(Temperature_C),
-                       Tmax_temperature = max(Temperature_C),
+      dplyr::filter(!is.na({{aggregation_column}})) %>%
+      dplyr::summarise(mean_value = mean({{aggregation_column}}),
+                       std_value = stats::sd({{aggregation_column}}),
+                       min_value = min({{aggregation_column}}),
+                       max_value = max({{aggregation_column}}),
                        Logger_ID = dplyr::first(Logger_ID),
                        .groups = "drop")
-    return(seasonal_temperature)
+    return(seasonal_data)
   } else {
     stop("Invalid aggregation_type. Please choose 'daily', 'monthly', 'seasonal', or 'annual'.")
   }
 }
-
