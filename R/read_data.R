eb0759d2d35664df80be5e266eccc288822e0af2
Ela <ElenaScholz>
2024-08-15 11:30:38 +0200

    update again

--- R/read_data.R
+++ R/read_data.R
@@ -1,18 +1,3 @@
-#' read_data
-#' reads in files used for the analysis of logger data. Possible file types are: Excel, csv or a dataframe
-#' @param input_directory MANDATORY, Type: String - The input directory where the file(s) is/are stored
-#' @param skip_lines OPTIONAL, Type: Integer - If your csv file has some lines above the actual data, change the parameter from 0 to the number of lines that need to be skipped.
-#' @param csv_sep MANDATORY, Type: String - The csv seperator used in the file. If no
-#' @param csv_comment_character OPTIONAL, Type: String - default values: ","
-#' @param add_ID_from_filename OPTIONAL, Type: TRUE/FALSE - default value: TRUE
-#' @param index_id OPTIONAL, Type: vector - should contain the start and end position of the ID in the filename, default: c(0,6)
-#'
-#' @return a single dataframe or a list of dataframes containing the raw data
-#'
-#'
-#' @export
-#'
-
 read_data <- function(input_directory, skip_lines = 0, csv_sep = ",", csv_comment_character = '#', add_ID_from_filename = TRUE, index_id = c(0, 6)) {
 
   check_path(input_directory)
@@ -22,16 +7,16 @@ read_data <- function(input_directory, skip_lines = 0, csv_sep = ",", csv_commen
 
   if (length(files) == 1) {
     check_file(paste0(input_directory, files))
-    dataframe <- check_format(paste0(input_directory, files), skip_lines = skip_lines)
+    dataframe <- check_format(paste0(input_directory, files), skip_lines = skip_lines, csv_sep = csv_sep, comment.char = csv_comment_character)
     if (add_ID_from_filename) {
       id_logger <- substr(files, index_id[1], index_id[2])
-      dataframe$Logger.ID <- id_logger
+      dataframe$Logger_ID <- id_logger
     }
     return(dataframe)
   } else {
     for (i in files) {
       check_file(paste0(input_directory, i))
-      dataframe <- check_format(paste0(input_directory, i))
+      dataframe <- check_format(paste0(input_directory, i), skip_lines = skip_lines, csv_sep = csv_sep, comment.char = csv_comment_character)
       if (add_ID_from_filename) {
         id_logger <- substr(i, index_id[1], index_id[2])
         dataframe$Logger_ID <- id_logger
@@ -41,4 +26,3 @@ read_data <- function(input_directory, skip_lines = 0, csv_sep = ",", csv_commen
     return(logger_dataframes)
   }
 }
-
