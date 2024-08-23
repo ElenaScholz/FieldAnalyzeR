944a55a2a7125b8122a8f7e5e5e287b8865f8f4f
Ela <ElenaScholz>
2024-08-23 14:29:29 +0200

    file_pattern

--- R/read_data.R
+++ R/read_data.R
@@ -13,12 +13,12 @@
 #'
 #' @export
 #'
-read_data <- function(input_directory, file_pattern = NULL, skip_lines = 0, csv_sep = ",", csv_comment_character = '#', add_ID_from_filename = TRUE, index_id = c(0, 6)) {
+read_data <- function(input_directory, file_pattern = FALSE, skip_lines = 0, csv_sep = ",", csv_comment_character = '#', add_ID_from_filename = TRUE, index_id = c(0, 6)) {
 
 
   check_path(input_directory)
 
-  if (file_pattern is.NULL){
+  if (file_pattern == FALSE){
     files <- list.files(input_directory)
   } else{}
   files <- list.files(input_directory, pattern = file_pattern)}
